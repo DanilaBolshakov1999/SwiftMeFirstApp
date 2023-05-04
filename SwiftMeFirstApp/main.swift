7f61380988597daac849d9f2e95f8824a921d1d7
Danila Bolshakov <danila1999777@gmail.com>
2023-05-04 21:39:33 +0300

    release v4

--- SwiftMeFirstApp/main.swift
+++ SwiftMeFirstApp/main.swift
@@ -7,21 +7,8 @@
 
 import Foundation
 
-var num1: String? //запрос первого аргумента, query first argument
-var num2: String? //запрос второго аргумента, query first argument
+let firstArgument = getArgumentFromConsole(argumentNumber: 1) //запись в переменную func(_), запрос первого аргумента
+let secondArgument = getArgumentFromConsole(argumentNumber: 2) //запись в переменную func(_), запрос второго аргумента
+var result = firstArgument + secondArgument //подсчет суммы аргументов
 
-repeat { //repeat {} while
-    print("Введите числовое значение первого аргумента") //print in console
-    num1 = readLine() //метод для использования консоли для ввода, method to use console for input
-} while Int(num1!) == nil //while проверка, while examination
-
-repeat { //repeat {} while
-    print("Введите числовое значение второго аргумента") //print in console
-    num2 = readLine() //метод для использования консоли для ввода, method to use console for input
-} while Int(num2!) == nil // while проверка, while examination
-
-//подсчет суммы аргументов
-var result = sum(num1, num2)
-
-//вывод результата на консоль
-print("Результат сложения - \(result)")
+print("Результат сложения - \(result)") //вывод результата на консоль
