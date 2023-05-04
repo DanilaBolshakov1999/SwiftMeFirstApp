6ad61e65caf0b78a66239b3f17ed9d09ce0e66fb
Danila Bolshakov <danila1999777@gmail.com>
2023-05-04 20:41:17 +0300

    BugFix release v3

--- SwiftMeFirstApp/main.swift
+++ SwiftMeFirstApp/main.swift
@@ -7,18 +7,21 @@
 
 import Foundation
 
-var num1: String?
-repeat {
-    print("Введите числовое значение первого аргумента")
-    num1 = readLine()
-} while Int(num1!) == nil
-//запрос второго аргумента
-var num2: String?
-repeat {
-    print("Введите числовое значение второго аргумента")
-    num2 = readLine()
-} while Int(num2!) == nil
+var num1: String? //запрос первого аргумента, query first argument
+var num2: String? //запрос второго аргумента, query first argument
+
+repeat { //repeat {} while
+    print("Введите числовое значение первого аргумента") //print in console
+    num1 = readLine() //метод для использования консоли для ввода, method to use console for input
+} while Int(num1!) == nil //while проверка, while examination
+
+repeat { //repeat {} while
+    print("Введите числовое значение второго аргумента") //print in console
+    num2 = readLine() //метод для использования консоли для ввода, method to use console for input
+} while Int(num2!) == nil // while проверка, while examination
+
 //подсчет суммы аргументов
 var result = sum(num1, num2)
+
 //вывод результата на консоль
 print("Результат сложения - \(result)")
