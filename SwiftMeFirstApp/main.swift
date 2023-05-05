ceb728941628db175ad5c497739028a9f75e8344
Danila Bolshakov <danila1999777@gmail.com>
2023-05-05 20:43:10 +0300

    release v4

--- SwiftMeFirstApp/main.swift
+++ SwiftMeFirstApp/main.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 
+let availableOperations = ["*", "/", "+", "-", "^"] // массив содержит список доступных операций
+let operation = getOperationFromConsole() // запрос операции
 let firstArgument = getArgumentFromConsole(argumentNumber: 1) //запись в переменную func(_), запрос первого аргумента
 let secondArgument = getArgumentFromConsole(argumentNumber: 2) //запись в переменную func(_), запрос второго аргумента
 var result = firstArgument + secondArgument //подсчет суммы аргументов
